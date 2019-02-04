6ba2ade7dd8326388d10d110360cfdd0ce97c26d
Edem <keltikan@gmail.com>
2019-02-04 23:27:14 +0300

    Multiple user support

--- main.rb
+++ main.rb
@@ -11,28 +11,38 @@ LEFT_CARDS = ENV['LEFT_CARDS'].freeze
 EMPTY_DECK = ENV['EMPTY_DECK'].freeze
 HERE_YOU_ARE = ENV['HERE_YOU_ARE'].freeze
 
-cards = []
+cards = {}
+decks = {}
 
-Telegram::Bot::Client.run(TOKEN) do |bot|
-  bot.listen do |message|
-    if message.text == '/start'
-      deck = Deck.new.shuffle
-      cards = deck.cards
-      bot.api.send_message(chat_id: message.chat.id, text: "#{NEW_DECK} #{HOW_MANY}")
-    else
-      if message.text.to_i <= 0 && cards.any?
-        bot.api.send_message(chat_id: message.chat.id, text: "#{WRONG_INPUT} #{HOW_MANY}")
-      else
-        number = message.text.to_i
-        if number <= cards.count
-          number.times { bot.api.send_message(chat_id: message.chat.id, text: cards.pop) }
-          bot.api.send_message(chat_id: message.chat.id, text: "#{HOW_MANY} #{cards.size} #{LEFT_CARDS}") if cards.any?
+
+begin
+  Telegram::Bot::Client.run(TOKEN) do |bot|
+    bot.listen do |rqst|
+      Thread.start(rqst) do |rqst|
+        if rqst.text == '/start'
+          decks[rqst.chat.id] = Deck.new.shuffle
+          cards[rqst.chat.id] = decks[rqst.chat.id].cards
+          bot.api.send_message(chat_id: rqst.chat.id, text: "#{NEW_DECK} #{HOW_MANY}")
         else
-          bot.api.send_message(chat_id: message.chat.id, text: "#{cards.size} #{LEFT_CARDS} #{HERE_YOU_ARE}") if cards.any?
-          cards.size.times { bot.api.send_message(chat_id: message.chat.id, text: cards.pop) }
+          if rqst.text.to_i <= 0 && cards[rqst.chat.id].any?
+            bot.api.send_message(chat_id: rqst.chat.id, text: "#{WRONG_INPUT} #{HOW_MANY}")
+          else
+            number = rqst.text.to_i
+            if number <= cards[rqst.chat.id].count
+              number.times { bot.api.send_message(chat_id: rqst.chat.id, text: cards[rqst.chat.id].pop) }
+              bot.api.send_message(chat_id: rqst.chat.id, text: "#{HOW_MANY} #{cards[rqst.chat.id].size} #{LEFT_CARDS}") if cards[rqst.chat.id].any?
+            else
+              bot.api.send_message(chat_id: rqst.chat.id, text: "#{cards[rqst.chat.id].size} #{LEFT_CARDS} #{HERE_YOU_ARE}") if cards[rqst.chat.id].any?
+              cards[rqst.chat.id].size.times { bot.api.send_message(chat_id: rqst.chat.id, text: cards[rqst.chat.id].pop) }
+            end
+          end
+        bot.api.send_message(chat_id: rqst.chat.id, text: EMPTY_DECK) if cards[rqst.chat.id].empty?
         end
       end
-    bot.api.send_message(chat_id: message.chat.id, text: EMPTY_DECK) if cards.empty?
     end
   end
+rescue Exception => e
+  File.write("#{__dir__}/errors.log", "#{Time.now}\n#{e.message}\n", mode: 'a')
+  sleep 60
+  retry
 end
